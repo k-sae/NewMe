6e51eeb737cdba4f893fb2d8350fd4fff690b101
kareem <karim.salah2048@gmail.com>
2017-07-28 00:51:51 +0200

    add postconnector class for sending post requests

--- app/src/main/java/com/kareem/newme/News/NewsDetailsFragment.java
+++ app/src/main/java/com/kareem/newme/News/NewsDetailsFragment.java
@@ -8,8 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.kareem.newme.Connections.PostConnector;
 import com.kareem.newme.Constants;
 import com.kareem.newme.R;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -23,7 +29,24 @@ public class NewsDetailsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         String jsonNews = getActivity().getIntent().getStringExtra(Constants.NEWS_DATA);
-        Log.e("NewsDetails", "onCreateView: " + jsonNews );
-        return inflater.inflate(R.layout.fragment_news_details, container, false);
+        View view = inflater.inflate(R.layout.fragment_news_details, container, false);
+        try {
+            PostConnector postConnector = new PostConnector(new URL("http://drhanadi.com/newmemobile/classes/MobileApi.php")) {
+                @Override
+                public void onFinish(String data) {
+                    Log.e("asd", "onFinish: "+ data);
+                }
+            };
+            Map<String,String> params = new HashMap<>();
+//            params.put("req","addNew");
+//            params.put("new", jsonNews);
+            params.put("req", "deleteNew");
+            params.put("newId", "-Kq5Br7peEFznzrrj2OY");
+            postConnector.setParams(params);
+            postConnector.execute();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        return view;
     }
 }
