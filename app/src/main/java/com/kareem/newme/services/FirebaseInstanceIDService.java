fd0025d25d344e1fc008a5cf4bbf7a75950a630f
kareem <karim.salah2048@gmail.com>
2017-08-05 22:30:55 +0200

    add firebase messaging services

---
+++ app/src/main/java/com/kareem/newme/services/FirebaseInstanceIDService.java
@@ -0,0 +1,55 @@
+package com.kareem.newme.services;
+
+
+        import android.app.AlertDialog;
+        import android.content.DialogInterface;
+        import android.util.Log;
+
+        import com.google.firebase.iid.FirebaseInstanceId;
+
+public class FirebaseInstanceIDService extends com.google.firebase.iid.FirebaseInstanceIdService {
+
+    private static final String TAG = "MyFirebaseIIDService";
+
+    @Override
+    public void onTokenRefresh() {
+
+        //TODO on recive send it to the server and save it to local database 
+        //Getting registration token
+        String refreshedToken = FirebaseInstanceId.getInstance().getToken();
+//dialogBox(refreshedToken);
+        //Displaying token in logcat
+        Log.e(TAG, "Refreshed token: " + refreshedToken);
+
+    }
+
+    private void sendRegistrationToServer(String token) {
+        //You can implement this method to store the token on your server
+        //Not required for current project
+    }
+
+    public void dialogBox(String message) {
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage(message);
+        alertDialogBuilder.setPositiveButton("Ok",
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+                    }
+                });
+
+        alertDialogBuilder.setNegativeButton("cancel",
+                new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface arg0, int arg1) {
+
+                    }
+                });
+
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+    }
+
+}
---
+++ app/src/main/java/com/kareem/newme/services/FirebaseMessagingService.java
@@ -0,0 +1,69 @@
+package com.kareem.newme.services;
+
+
+        import android.app.Notification;
+        import android.app.NotificationManager;
+        import android.app.PendingIntent;
+        import android.content.Context;
+        import android.content.Intent;
+        import android.graphics.Color;
+        import android.media.RingtoneManager;
+        import android.net.Uri;
+        import android.support.v4.app.NotificationCompat;
+        import android.util.Log;
+
+        import com.google.firebase.messaging.RemoteMessage;
+        import com.kareem.newme.MainActivity;
+        import com.kareem.newme.R;
+
+
+public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
+
+    private static final String TAG = "Android News App";
+
+    @Override
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+
+        //It is optional
+        Log.e(TAG, "From: " + remoteMessage.getFrom());
+        Log.e(TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
+
+        //Calling method to generate notification
+        sendNotification(remoteMessage.getNotification().getTitle(), remoteMessage.getNotification().getBody());
+    }
+
+    //This method is only generating push notification
+    private void sendNotification(String title, String messageBody) {
+
+
+        long[] vibrate  = {500,200,200,500};
+        Uri alarmSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent,
+                PendingIntent.FLAG_ONE_SHOT);
+
+        Uri defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this)
+                .setDefaults(Notification.DEFAULT_ALL)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(alarmSound)
+                .setLights(Color.CYAN, 1, 1)
+                .setVisibility(Notification.VISIBILITY_PUBLIC)
+                .setPriority(1000)
+                .setVibrate(vibrate)
+                .setContentIntent(pendingIntent);
+
+        NotificationManager notificationManager =
+                (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+
+        notificationManager.notify(0, notificationBuilder.build());
+    }
+
+
+
+
+}
